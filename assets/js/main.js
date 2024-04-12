0970167d9273eab3c70fd1579e2abdcf22daefec
dfhoover <163333700+dfhoover@users.noreply.github.com>
2024-04-12 19:09:09 -0400

    0412.7pm common
    still working on common

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,5 @@
 /**
 * Template Name: ZenBlog
-* Template URL: https://bootstrapmade.com/zenblog-bootstrap-blog-template/
-* Author: BootstrapMade.com
-* License: https:///bootstrapmade.com/license/
 */
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
