e4bb9f5238fece4c980e6a28d4c5013f13de3e4c
dfhoover <163333700+dfhoover@users.noreply.github.com>
2024-04-14 19:35:54 -0400

    0414.735pm
    common crane test

--- commonPost.js
+++ commonPost.js
@@ -1,12 +1,40 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function() {
-  const headerElement = document.getElementById('header-element');
+ //const headerElement = document.getElementById('header-element');
+ const headerElement = document.getElementById('common-side-popular-element');
 
   // Fetch the content of the header file
+  //fetch('../commonPost.html')
+  fetch('../commonSidePopular.html')
+    .then(response => response.text())
+    .then(data => {
+      headerElement.innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error fetching header:', error);
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+  const headerElement = document.getElementById('header-element');
   fetch('../commonPost.html')
     .then(response => response.text())
     .then(data => {
-      // Insert the header content into the placeholder element
+      headerElement.innerHTML = data;
+    })
+    .catch(error => {
+      console.error('Error fetching header:', error);
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+ //const headerElement = document.getElementById('header-element');
+ const headerElement = document.getElementById('common-side-element');
+
+  // Fetch the content of the header file
+  //fetch('../commonPost.html')
+  fetch('../commonSide.html')
+    .then(response => response.text())
+    .then(data => {
       headerElement.innerHTML = data;
     })
     .catch(error => {
